fa87302b1e7d708220da60d6c415488430bbe884
shepsito <shepsi@gmail.com>
2026-07-25 23:43:41 +0300

    Enhance event generation with HTML formatting
    Refactor event generation logic to include HTML formatting for event descriptions.

--- service_logic.py
+++ service_logic.py
@@ -12,6 +12,144 @@ def get_week_of_month(date):
         return 0
     delta_days = (date - first_monday).days
     return (delta_days // 7) + 1
+from datetime import datetime, timedelta
+import calendar
+
+def get_week_of_month(date):
+    first_day = date.replace(day=1)
+    first_weekday = first_day.weekday()
+    if first_weekday == 0:
+        first_monday = first_day
+    else:
+        first_monday = first_day + timedelta(days=(7 - first_weekday))
+    if date < first_monday:
+        return 0
+    delta_days = (date - first_monday).days
+    return (delta_days // 7) + 1
+
+def is_last_monday_of_quarter(date):
+    return date.weekday() == 0 and date.month in [3,6,9,12] and calendar.monthrange(date.year,date.month)[1] - date.day < 7
+
+def is_last_tuesday_of_quarter(date):
+    return date.weekday() == 1 and date.month in [3,6,9,12] and calendar.monthrange(date.year,date.month)[1] - date.day < 7
+
+def is_last_wednesday_of_quarter(date):
+    return date.weekday() == 2 and date.month in [3,6,9,12] and calendar.monthrange(date.year,date.month)[1] - date.day < 7
+
+def is_last_thursday_of_quarter(date):
+    return date.weekday() == 3 and date.month in [3,6,9,12] and calendar.monthrange(date.year,date.month)[1] - date.day < 7
+
+def is_last_friday_of_quarter(date):
+    return date.weekday() == 4 and date.month in [3,6,9,12] and calendar.monthrange(date.year,date.month)[1] - date.day < 7
+
+# HTML ЧЕРВЕН ЦВЯТ
+RED = '<span style="color:#ff0000;">НСЕО,ОЕОи СКУ</span>'
+RED_NSEO = '<span style="color:#ff0000;">НСЕО</span>'
+RED_DIS = '<span style="color:#ff0000;">ДИС,ОЕОи СКУ</span>'
+RED_ENERG = '<span style="color:#ff0000;">НСЕО,ЕнергетикПРАО</span>'
+
+def generate_yearly_schedule(year):
+    events = []
+    current = datetime(year,1,1)
+    end = datetime(year,12,31)
+
+    while current <= end:
+        day = current.day
+        month = current.month
+        week = get_week_of_month(current)
+
+        def night_event(title, facility, description):
+            return {
+                'datetime': (current - timedelta(days=1)).replace(hour=23),
+                'title': title,
+                'facility': facility,
+                'description': description,
+                'shift': 'Смяна 1'
+            }
+
+        def morning_event(title, facility, description):
+            return {
+                'datetime': current.replace(hour=7),
+                'title': title,
+                'facility': facility,
+                'description': description,
+                'shift': 'Смяна 2'
+            }
+
+        def afternoon_event(title, facility, description):
+            return {
+                'datetime': current.replace(hour=15),
+                'title': title,
+                'facility': facility,
+                'description': description,
+                'shift': 'Смяна 3'
+            }
+
+        # --- ВСИЧКИ УСЛОВИЯ С HTML ЧЕРВЕН ЦВЯТ ---
+
+        if month in [2,9] and current.weekday()==0 and week==1:
+            events.append(afternoon_event(' Проверка АВР','Аварийно осветление',f'Проверка АВР на захранването-{RED}'))
+
+        if day in [11,12]:
+            events.append(afternoon_event(' ЕЕ ЦПС-1','ЕЕ ЦПС-1',f'Проверка изправноста на аварийно осветление-{RED}'))
+
+        if month in [3,10] and current.weekday()==0 and week in [1,2]:
+            events.append(morning_event(' Ф.И. Проверка','По процедура',f'Ф.И аварийно осветление-{RED}'))
+
+        if day == 15:
+            events.append(afternoon_event(' МЗ и ЕЕ ЦПС-1','МЗ и ЕЕ ЦПС-1',f'Проверка евакуационно осветление-{RED}'))
+
+        if is_last_monday_of_quarter(current):
+            events.append(afternoon_event(' Проверка АВР (Пон.)','МЗ,ЦПС-1',f'Проверка АВР на сборки 0.4кВ захранвани от 3 и 4БН-{RED}'))
+
+        if is_last_tuesday_of_quarter(current):
+            events.append(afternoon_event(' Проверка АВР (Вт.)','МЗ',f'Проверка АВР на сборки 0.4кВ захранвани от 23 и 24БН-{RED}'))
+
+        if is_last_wednesday_of_quarter(current):
+            events.append(afternoon_event(' Проверка АВР (Ср.)','МЗ',f'Проверка АВР на сборки 0.4кВ на съответната система I(II,III)-блок3-{RED}'))
+
+        if is_last_thursday_of_quarter(current):
+            events.append(afternoon_event(' Проверка АВР (Четв.)','МЗ',f'Проверка АВР на сборки 0.4кВ на съответната система I(II,III)-блок4-{RED}'))
+
+        if is_last_friday_of_quarter(current):
+            events.append(night_event(' Проверка АВР (Петък)','МЗ,ХВО и ЦПС-1',f'Проверка АВР на сборки 0.4кВ-{RED}'))
+
+        if day == 8:
+            events.append(night_event(' Секции 0,4кВ-ГК','Секции 0,4кВ-ГК',f'Проверка АВР на ШУ-{RED_DIS}'))
+
+        if day == 18:
+            events.append(afternoon_event(' Вентилни отводи','Вентилни отводи',f'Отчитане на вентилни отводи-{RED}'))
+
+        if day == 1:
+            events.append(night_event(' Ел.двигатели 6кВ','Ел.двигатели 6кВ',f'Измерване Riso на ел.двигатели 6кВ-{RED}'))
+            events.append(morning_event(' Отчитане електромери','Методика',f'Отчитане показанията на електромерите-{RED}'))
+
+        if month in [1,4,7,10] and current.weekday()==0 and week==1:
+            events.append(morning_event(' Проверка ДГ-А','ДГ-А',f'Ф.И на автономен товар ≥60мин-{RED}'))
+
+        if month in [1,4,7,10] and current.weekday()==0 and week==2:
+            events.append(morning_event(' Проверка ДГ-Б','ДГ-Б',f'Ф.И на автономен товар ≥60мин-{RED}'))
+
+        if month in [1,4,7,10] and current.weekday()==2 and week==3:
+            events.append(morning_event(' Проверка 2АДГ-ДСАПП-4','2АДГ-ДСАПП-4',f'Ф.И аварийно захранване-{RED_ENERG}'))
+
+        if month in [1,4,7,10] and current.weekday()==3 and week==3:
+            events.append(morning_event(' Проверка ДГ-КАС','ДГ-КАС',f'Ф.И аварийно захранване-{RED_ENERG}'))
+
+        if month in [6,12] and current.weekday()==0 and week==3:
+            events.append(morning_event(' Проверка ГРТ-ЦНРД','ГРТ-ЦНРД',f'Изпробване АВР-{RED_ENERG}'))
+
+        if current.weekday() == 5 and week == 3:
+            events.append(morning_event(' Проверка ТП1,ТП3','ТП1,ТП3',f'Изпробване вентилатори-{RED_NSEO}'))
+
+        if current.weekday() in [2,5] and week == 3:
+            events.append(night_event(' Измерване стойности по фидери','Методика',f'Измерване стойности-{RED}'))
+            events.append(morning_event(' Измерване стойности по фидери','Методика',f'Измерване стойности-{RED}'))
+            events.append(afternoon_event(' Измерване стойности по фидери','Методика',f'Измерване стойности-{RED}'))
+
+        current += timedelta(days=1)
+
+    return sorted(events, key=lambda x: x['datetime'])
 
 def is_last_monday_of_quarter(date):
     return date.weekday() == 0 and date.month in [3,6,9,12] and calendar.monthrange(date.year,date.month)[1] - date.day < 7
